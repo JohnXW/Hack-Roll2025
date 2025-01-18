d5e9b84a3ef0cff97465fad662a379ef23e061f5
xw Laptop <johntheghostie@gmail.com>
2025-01-18 19:36:35 +0800

    Wheel and colour

--- main.js
+++ main.js
@@ -9,7 +9,94 @@ let currTypeSize = 0;
 let callbacks = 0; //used to track, so that it will only call createOutput when all types in placeTypes are appeneded
 console.log(PlaceTypesDict["food"]);
 let outputted = false;
+
+//Wheel
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn")
 searchNearbyPlaces;
+const finalValue = document.getElementById("final-value");
+const rotationValues = [
+    {minDegree: 0, maxDegree: 30, value: 2},
+    {minDegree: 31, maxDegree: 90, value: 1},
+    {minDegree: 91, maxDegree: 150, value: 6},
+    {minDegree: 151, maxDegree: 210, value: 5},
+    {minDegree: 211, maxDegree: 270, value: 4},
+    {minDegree: 271, maxDegree: 330, value: 3},
+    {minDegree: 331, maxDegree: 360, value: 2},
+];
+
+const data = [16, 16, 16, 16, 16, 16];
+var pieColors = [
+    "#8b35bc",
+    "#b163da",
+    "#8b35bc",
+    "#b163da",
+    "#8b35bc",
+    "#b163da"
+];
+
+let myChart = new Chart (wheel, {
+    plugins: [ChartDataLabels],
+    type: "pie",
+    data: {
+        labels: [1,2,3,4,5,6],
+        datasets:[
+            {
+                backgroundColor: pieColors,
+                data: data,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        animation: {duration:0},
+        plugins: {
+            tooltip: false,
+            legend: {
+                display: false,
+            },
+            datalabels: {
+                color: "#000000",
+                formatter: (_, context) =>
+                context.chart.data.labels[context.dataIndex],
+                font: {size: 24},
+            },
+        },
+    },
+});
+const valueGenerator = (angleValue) => {
+    for (let i of rotationValues) {
+        if (angleValue >= i.minDegree && angleValue <= i.maxDegree){
+            finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+            spinBtn.disabled = false;
+            break;
+        }
+    }
+}
+//spinner count
+let count = 0;
+let resultValue = 101;
+spinBtn.addEventListener("click", () => {
+    spinBtn.disabled = true;
+    finalValue.innerHTML = `<p>Good Luck!</p>`;
+    let randomDegree = Math.floor(Math.random() * (355-0+1)+0);
+    let rotationInterval = window.setInterval(()=>{
+        myChart.options.rotation = myChart.options.rotation + resultValue;
+        myChart.update();
+        if(myChart.options.rotation >= 360) {
+            count += 1;
+            resultValue -= 5;
+            myChart.options.rotation = 0;
+        }
+        else if(count>15 && myChart.options.rotation == randomDegree){
+            valueGenerator(randomDegree);
+            clearInterval(rotationInterval);
+            count = 0;
+            resultValue = 101;
+        }
+    }, 10);
+});
+/////////////////////////////////////
 document.getElementById("type").onchange = searchNearbyPlaces;
 
 function initMap(){
@@ -19,9 +106,9 @@ function initMap(){
 
 function searchNearbyPlaces(){
     let selectedPlaceType = document.getElementById("type").value;
-    if(selectedPlaceType == "food") document.body.style.backgroundColor = "#b7c6d5";
-    else if(selectedPlaceType == "store") document.body.style.backgroundColor = "#AA0000";
-    else if(selectedPlaceType == "night") document.body.style.backgroundColor = "#000000";
+    if(selectedPlaceType == "food") document.body.style.background = "#b7c6d5";
+    else if(selectedPlaceType == "store") document.body.style.background = "#AA0000";
+    else if(selectedPlaceType == "night") document.body.style.background = "#000000";
 
     currTypeSize = PlaceTypesDict[selectedPlaceType].length;
     callbacks = 0;
